5ac6010b97544cbc879cf2595c1286d0070fe2fb
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-30 16:59:40 -0300

    Introduzido o menu para melhor entendimento do aplicativo.

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.IO.Compression;
+using System.Runtime.Intrinsics.X86;
 
 namespace RoboTupiniquim.ConsoleApp
 {
     internal class Program
     {
+        // Assuma que mover o robô para frente, significa mover sua posição de (X, Y)
+        // para(x, y+1). por exemplo, um robô na posição(0,0) com a face para o norte,
+        // ao se mover uma posição, vai parar em (0,1).
+
+        //        Entrada
+        //O programa deve possuir os seguintes parâmetros:
+        //● A primeira linha deve ser as coordenadas do canto superior direito da
+        //área.O canto inferior esquerdo é sempre(0,0).
+        //● O resto das entradas deve ser os comandos que o robô deve executar.
+        //Cada comando deve ser enviado em 2 partes: a primeira parte é a
+        //posição inicial do robô e a segunda uma série de instruções que aquele
+        //robô deve seguir para explorar a área.
+        //A posição é dada com 2 inteiros e uma letra, separados por espaços,
+        //correspondendo pelas coordenadas X e Y e a orientação do robô.Cada robô
+        //vai executar as instruções de forma sequencial: o segundo robô só iniciará
+        //suas ações quando o primeiro terminar.
+
         static void Main(string[] args)
 
             // menu inicial
@@ -13,21 +31,18 @@ namespace RoboTupiniquim.ConsoleApp
             Console.WriteLine("           Robô Tupiniquim");
             Console.WriteLine("--------------------------------------");
             Console.WriteLine();
-            Console.WriteLine("  Y");
-            Console.WriteLine("5 |");
-            Console.WriteLine("4 |");            
-            Console.WriteLine("3 |");
-            Console.WriteLine("2 |");
-            Console.WriteLine("1 |__.__.__.__.__ X");
-            Console.WriteLine("    1  2  3  4  5");
+            Console.WriteLine("Olá, e seja bem-vindo ao programa Robô Tupiniquim da AEB. Você precisará guiar o robô através das");
+            Console.WriteLine("câmeras acopladas nele. É importante informar que o robô irá se mover em uma superfície retangular,");
+            Console.WriteLine("sobre as quais você o guiará. Para efeitos de localização, o robô se moverá nas coordenadas X e Y, além de");
+            Console.WriteLine("receber uma letra que informará a direção para onde o robô estará olhando.");
             Console.WriteLine();
-            Console.WriteLine("Você é o controlador do robô. Para movê-lo, utilize os seguintes comandos:");
+            Console.WriteLine("Como você é o controlador do robô, é importante que você memorize os seguintes comandos:");
             Console.WriteLine("E: você fará com que o robô vire para a ESQUERDA do seu eixo, mas sem se mover.");
             Console.WriteLine("D: você fará com que o robô vire para a DIREITA do seu eixo, mas sem se mover.");
             Console.WriteLine("M: você moverá o robô para onde ele estiver apontando naquele momento.");
             Console.WriteLine();
 
-            // input: posição inicial / comandos
+            // input: posição inicial / GRID / comandos
 
             int positionX = 1;
             int positionY = 2;
